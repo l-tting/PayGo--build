8a3d6219a4a50b0e356676c62818570605a2fb65
l-tting <brianletting01@gmail.com>
2025-07-11 13:00:48 +0300

    user x auth

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter,status,Depends,HTTPException
+from fastapi import APIRouter,status,Depends,HTTPException,Response
 from werkzeug.security import generate_password_hash,check_password_hash
 from app.schemas import UserCreate,UserLogin
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
 from app.database import get_db
 from app.models import User
+from app.auth import create_access_token
+from datetime import timedelta
 
 router = APIRouter()
 
@@ -15,15 +18,57 @@ async def create_admin(user: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Admin already exists")
 
     hashed_password = generate_password_hash(user.password)
-    new_admin = User(
+    new_user = User(
         full_name=user.full_name,
         email=user.email,
-        admin_phone=admin.admin_phone,
+        phone_number=user.phone_number,
         password=hashed_password,
-      
     )
 
-    db.add(new_admin)
+    db.add(new_user)
     db.commit()
-    db.refresh(new_admin)
+    db.refresh(new_user)
     return {"message": "Admin created successfully"}
+
+@router.post("/login", status_code=status.HTTP_200_OK)
+def login_user(user_login: UserLogin, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email== user_login.email).first()
+    
+    if user is None or not check_password_hash(user.password, user_login.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    access_token = create_access_token(
+        data={"user": user_login.email}, 
+        expires_delta=timedelta(days=30)
+    )
+    print("Access token:", access_token)
+    
+    response = JSONResponse(
+        content={"message": "Login successful", "current_user": user.full_name}
+    )
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        max_age=30 * 24 * 60 * 60, 
+        expires=30 * 24 * 60 * 60,
+        samesite="Lax",             
+        secure=False                
+    )
+    return response
+
+
+
+@router.post("/logout", status_code=status.HTTP_200_OK)
+def logout_user(response: Response):
+    response.delete_cookie(
+        key="access_token",
+        httponly=True,
+        samesite="Lax",  
+        secure=False     # Set to True for HTTPS
+    )
+    return {"message": "Successfully logged out"}
+
+
+
+
--- app/schemas.py
+++ app/schemas.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class UserCreate(BaseModel):
     full_name:str
     email:str
+    phone_number:str
     password:str
 
 class UserLogin(BaseModel):
